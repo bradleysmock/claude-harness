0156de4fe6da42e9e904ced31f46117548a78619
Bradley Smock <bradleysmock@gmail.com>
2026-06-06 14:03:22 -0400

    cleanup

--- api/src/routes/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod animals;
-pub mod mill_inventory;
-pub mod preorders;
-pub mod sync;
-
-pub use animals::router as animals_router;
-pub use mill_inventory::router as mill_inventory_router;
-pub use preorders::router as preorders_router;
-pub use sync::sync_router;
